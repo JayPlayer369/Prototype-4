2e4798b135c56e630d655ecf2d64102d19358c9a
Ahacker1208 <97680603+Ahacker1208@users.noreply.github.com>
2024-01-08 12:37:46 -0500

    Finished 4.4
    Added multiple enemies

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -6,19 +6,32 @@ public class SpawnManager : MonoBehaviour
 {
     public float spawnRange = 9;
     public GameObject enemyPrefab;
+
+    public GameObject powerupPrefab;
+
+    public int enemyCount;
+    public int waveNumber = 1; 
+
     // Start is called before the first frame update
     void Start()
     {
-        Instantiate(enemyPrefab, new Vector3(0, 0, 6), enemyPrefab.transform.rotation);
-        float spawnPosX = Random.Range(-spawnRange, spawnRange);
-        float spawnPosZ = Random.Range(-spawnRange, spawnRange);
-        Vector3 randomPos = new Vector3(spawnPosX, 0, spawnPosZ);
+        SpawnEnemyWave(waveNumber);
+       Instantiate(powerupPrefab, GenerateSpawnPosition(), powerupPrefab.transform.rotation);
+
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        enemyCount = FindObjectsOfType<Enemy>().Length;
+        if (enemyCount == 0) 
+        {
+            waveNumber++; 
+            SpawnEnemyWave(waveNumber);
+            Instantiate(powerupPrefab, GenerateSpawnPosition(), powerupPrefab.transform.rotation); 
+        }
+       
     }
 
     public Vector3 GenerateSpawnPosition ()
@@ -27,5 +40,13 @@ public class SpawnManager : MonoBehaviour
        float spawnPosZ = Random.Range(-spawnRange, spawnRange);
        Vector3 randomPos = new Vector3(spawnPosX, 0, spawnPosZ);
        return randomPos;
+    }
+
+    void SpawnEnemyWave(int enemiesToSpawn)
+    {
+        for (int i = 0; i < enemiesToSpawn; i++)
+        {
+            Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
+        }
     }
 }
